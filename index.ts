8d03a1e2fb59c61a29ad4938cd8ad18dd373ef87
nitzan984 <nitzan984@gmail.com>
2023-05-04 10:44:53 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -5,22 +5,26 @@ import { Observable, Subscriber } from 'rxjs';
 
 //Cold Observable
 
-// ajax('https://random-data-api.com/api/v2/users?size=2&is_xml=true').subscribe(data=>{
-//   console.log(data.response[0].first_name);
-// });
+ajax('https://random-data-api.com/api/v2/users?size=2&is_xml=true').subscribe(
+  (data) => {
+    console.log(data.response[0].first_name);
+  }
+);
 
-// ajax('https://random-data-api.com/api/v2/users?size=2&is_xml=true').subscribe(data=>{
-//   console.log(data.response[0].first_name);
-// });
+ajax('https://random-data-api.com/api/v2/users?size=2&is_xml=true').subscribe(
+  (data) => {
+    console.log(data.response[0].first_name);
+  }
+);
 
 //Hot Observable
 
-// const button = document.querySelector('button');
-// const obsClick$ = new Observable<MouseEvent>(subscriber=>{
-//     button.addEventListener('click', event =>{
-//     subscriber.next(event);
-//   });
-// });
+const button = document.querySelector('button');
+const obsClick$ = new Observable<MouseEvent>((subscriber) => {
+  button.addEventListener('click', (event) => {
+    subscriber.next(event);
+  });
+});
 
-// obsClick$.subscribe((event) =>console.log(event.type,event.x,event.y));
-// obsClick$.subscribe((event) =>console.log(event.type,event.x,event.y));
+obsClick$.subscribe((event) => console.log(event.type, event.x, event.y));
+obsClick$.subscribe((event) => console.log(event.type, event.x, event.y));
